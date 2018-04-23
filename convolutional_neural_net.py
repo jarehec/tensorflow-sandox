0a9a6e9767e2d4ff2cd9576cf34f7530732e5f06
jarehec <bone1198@gmail.com>
2018-04-23 10:01:36 -0700

    using tf.estimator api

--- convolutional_neural_net.py
+++ convolutional_neural_net.py
@@ -1,69 +1,134 @@
 # Convolution -> Pooling -> Convolution -> Pooling
 # -> Fully Connected Layer -> Output
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('/tmp/data', one_hot=True)
 
-n_classes = 10
+img_size = 28
+img_size_flat = img_size * img_size
+img_shape = (img_size, img_size)
+
 batch_size = 128
+n_classes = 10
+n_channels = 1
+num_epochs = 10
 
-x = tf.placeholder('float', [None, 784])
-y = tf.placeholder('float', [None, n_classes])
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+        # Show the classes as the label on the x-axis.
+        ax.set_xlabel(xlabel)
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
 
 def max_pool_2d(x):
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    return tf.layers.max_pooling2d(inputs=x, pool_size=2, strides=2)
 
-def convolutional_neural_net(data):
-    weights = {
-        # 5x5 convolution, 1 input, 32 outputs
-        'W_conv1': tf.Variable(tf.random_normal([5,5,1,32])),
-        # 5x5 convolution, 32 inputs, 64 outputs
-        'W_conv2': tf.Variable(tf.random_normal([5,5,32,64])),
-        # fully-connected, 7*7*64 inputs, 1024 outputs
-        'W_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
-        # 1024 inputs, n_classes outputs (class prediction)
-        'out': tf.Variable(tf.random_normal([1024, n_classes]))
-    }
-    biases = {
-        'b_conv1': tf.Variable(tf.random_normal([32])),
-        'b_conv2': tf.Variable(tf.random_normal([64])),
-        'b_fc': tf.Variable(tf.random_normal([1024])),
-        'out': tf.Variable(tf.random_normal([n_classes]))
-    }
-    data = tf.reshape(data, shape=[-1, 28, 28, 1])
+def convolutional_model_fn(features, labels, mode, params):
+    x = features['x']
+    cnn = tf.reshape(x, shape=[-1, img_size, img_size, n_channels])
+    # conv1 -> pool1 -> conv2 -> pool2
+    cnn = tf.layers.conv2d(inputs=cnn, name='conv1_layer',
+                           filters=16, kernel_size=4,
+                           padding='SAME', activation=tf.nn.relu)
+    cnn = max_pool_2d(cnn)
+    cnn = tf.layers.conv2d(inputs=cnn, name='conv2_layer',
+                           filters=36, kernel_size=4,
+                           padding='SAME', activation=tf.nn.relu)
+    cnn = max_pool_2d(cnn)
+    # flatten cnn
+    cnn = tf.layers.flatten(cnn)
+    # fully-connected layer
+    cnn = tf.layers.dense(inputs=cnn, name='fc1_layer',
+                          units=128, activation=tf.nn.relu)
+    # second fully-connected layer
+    cnn = tf.layers.dense(inputs=cnn, name='fc2_layer', units=10)
+    # logits output of cnn
+    logits = cnn
+    # Softmax output of the neural network.
+    prediction = tf.nn.softmax(logits=logits)
+    # Classification output of the neural network.
+    pred_class = tf.argmax(prediction, axis=1)
 
-    conv1 = tf.nn.relu(conv2d(data, weights['W_conv1']) + biases['b_conv1'])
-    conv1 = max_pool_2d(conv1)
-    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
-    conv2 = max_pool_2d(conv2)
-    # Reshape conv2 output to fit fully connected layer
-    fully_connected = tf.reshape(conv2, [-1,7*7*64])
-    fully_connected = tf.nn.relu(tf.matmul(fully_connected, weights['W_fc']) + biases['b_fc'])
-    output = tf.matmul(fully_connected, weights['out']) + biases['out']
-    return output
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        spec = tf.estimator.EstimatorSpec(mode=mode, predictions=pred_class)
+    else:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                       labels=labels)
+        loss = tf.reduce_mean(cross_entropy)
+        optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
+        train_op = optimizer.minimize(loss=loss,
+                                      global_step=tf.train.get_global_step())
+        # Eval metrics
+        eval_metrics = {'accuracy': tf.metrics.accuracy(labels=labels,
+                                                        predictions=pred_class)}
+        spec = tf.estimator.EstimatorSpec(mode=mode, loss=loss,
+                                          train_op=train_op,
+                                          eval_metric_ops=eval_metrics)
+    return spec
 
-def train_neural_network(x):
-    prediction = convolutional_neural_net(x)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
-    n_epochs = 10
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(n_epochs):
-            epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples / batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, loss], feed_dict={x:epoch_x, y:epoch_y})
-                epoch_loss += c
-            # TODO print batch completion
-            print('Epoch', epoch + 1, 'completed out of', n_epochs, '\nloss:', epoch_loss)
-        # This will tell us how many predictions we made that were perfect matches to their labels.
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
 
-train_neural_network(x)
+def main(_):
+    mnist.train.cls = np.argmax(mnist.train.labels, axis=1)
+    mnist.test.cls = np.argmax(mnist.test.labels, axis=1)
+    mnist.validation.cls = np.argmax(mnist.validation.labels, axis=1)
+    some_images = mnist.validation.images[0:9]
+    params = {'learning_rate': 1e-3}
+    
+    # Get the first images from the test-set.
+    images = mnist.test.images[0:9]
+    # Get the true classes for those images.
+    cls_true = mnist.test.cls[0:9]
+    # Plot the images and labels using our helper-function above.
+    plot_images(images=images, cls_true=cls_true)
+    
+    mnist_classifier = tf.estimator.Estimator(model_fn=convolutional_model_fn,
+                                              params=params,
+                                              model_dir='/tmp/mnist_cnn')
+    # train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(x={'x': np.array(mnist.train.images)},
+                                                        y=np.array(mnist.train.cls),
+                                                        batch_size=batch_size,
+                                                        num_epochs=None,
+                                                        shuffle=True)
+
+    # evaluate the model and print results
+    test_input_fn = tf.estimator.inputs.numpy_input_fn(x={'x': np.array(mnist.test.images)},
+                                                       y= np.array(mnist.test.cls),
+                                                       num_epochs=1,
+                                                       shuffle=False)
+    # predict class
+    predict_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": some_images},
+                                                          num_epochs=1,
+                                                          shuffle=False)
+    
+    mnist_classifier.train(input_fn=train_input_fn, steps=500)
+    test_results = mnist_classifier.evaluate(input_fn=test_input_fn)
+    print(test_results)
+    predictions = mnist_classifier.predict(input_fn=predict_input_fn)
+    cls_pred = np.array(list(predictions))
+    print(cls_pred)
+
+    plot_images(images=some_images,
+            cls_true=mnist.validation.cls[:9],
+            cls_pred=cls_pred)
+    
+if __name__ == '__main__':
+    tf.app.run()
